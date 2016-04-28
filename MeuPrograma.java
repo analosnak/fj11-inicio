a754a0090370ee6b4f47120d4b6b2ebb46107913
Ana Losnak <ana.losnak@caelum.com.br>
2016-04-28 17:07:25 -0300

    Erro-sem static

--- MeuPrograma.java
+++ MeuPrograma.java
@@ -1,5 +1,5 @@
 class MeuPrograma {
-  public static void main(String[] args) {
-    System.out.println("Sem ponto e virgula")
+  public void main(String[] args) {
+    System.out.println("Sem ponto e virgula");
   }
 }
